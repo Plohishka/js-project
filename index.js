3f2546d56ff24aa306657f30a635e819ad1c79ea
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-06-18 18:43:28 +0200

    Collection animals

--- index.js
+++ index.js
@@ -2,42 +2,77 @@
 
 
 
-class Human {
-    constructor(fullName, year, gender) {
-
-    this.fullName = fullName;
-    this.year = year;
-    this.gender = gender;
-
+class CollectionAnimals {
+    constructor(animals) {
+        this.animals = [];
     }
 
-    greeting() {
-        return this.gender === 'Mr' ? `Hello Mr. ${this.fullName}!` : `Hello Mrs. ${this.fullName}!`;
+    #add(animal) {
+         this.animals.push(animal);
     }
+
+    #createAnimal(animal) {
+        switch(animal) {
+            case 'cow': return collection.#add(new Cow);
+            case 'goat': return collection.#add(new Goat);
+            case 'cat': return collection.#add(new Cat);
+            case 'dog': return collection.#add(new Dog);
+            default: throw new TypeError('Unknown type of animal!');
+        }
+    }
+
+    createAmountAnimal(name, animal, child, childOfChild) {
+        if (child === undefined) {
+            child = 0;
+        }
+
+        if (childOfChild === undefined) {
+            childOfChild = 0;
+        }
+       
+        for (let i = 1; i <= (animal + child + childOfChild); i++) {
+            this.#createAnimal(name);
+        }
+    }
+
+    countTotal() {
+        return this.animals.length;
+    }
+
 }
 
-class Student extends Human {
-    constructor(fullName, year, gender, admissionYear, recordBook, averageScore) {
-    super(fullName, year, gender);
+const collection = new CollectionAnimals;
 
-    this.admissionYear = admissionYear;
-    this.recordBook = recordBook;
-    this.averageScore = averageScore;
 
-    }
-
-    isExcellentStudent() {
-        const date = new Date();
-        this.averageScore = this.recordBook / (date.getFullYear() - this.admissionYear);
-        return this.averageScore >= 90;
-    }
+class Cow {
 
 }
 
-const people1 = new Human('Anzhelika Plokhykh', 1989, 'Mrs');
-const student1 = new Student('Oleksandr Plokhykh', 1989, 'Mr', 2020, 300);
+class Goat {
 
-console.log(people1);
-console.log(people1.greeting());
-console.log(student1);
-console.log(student1.isExcellentStudent());
+}
+
+class Cat {
+
+}
+
+class Dog {
+
+}
+
+
+collection.createAmountAnimal('cow', 2, 3, 5);
+collection.createAmountAnimal('goat', 2, 4);
+collection.createAmountAnimal('cat', 3);
+collection.createAmountAnimal('dog', 2);
+
+console.log(collection);
+console.log(collection.countTotal());
+
+
+
+
+
+
+
+
