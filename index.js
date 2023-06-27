c89d0c1976f58ccc5654f288874ed931867d9b4c
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-06-27 21:50:36 +0200

    home work toggle

--- index.js
+++ index.js
@@ -2,72 +2,12 @@
 
 
 
-class CollectionAnimals {
-    constructor(animals) {
-        this.animals = [];
-    }
+const btn = document.querySelector('#btn');
+const body = document.querySelector('body');
 
-    #add(animal) {
-         this.animals.push(animal);
-    }
-
-    #createAnimal(animal) {
-        switch(animal) {
-            case 'cow': return collection.#add(new Cow);
-            case 'goat': return collection.#add(new Goat);
-            case 'cat': return collection.#add(new Cat);
-            case 'dog': return collection.#add(new Dog);
-            default: throw new TypeError('Unknown type of animal!');
-        }
-    }
-
-    createAmountAnimal(name, animal, child, childOfChild) {
-        if (child === undefined) {
-            child = 0;
-        }
-
-        if (childOfChild === undefined) {
-            childOfChild = 0;
-        }
-       
-        for (let i = 1; i <= (animal + child + childOfChild); i++) {
-            this.#createAnimal(name);
-        }
-    }
-
-    countTotal() {
-        return this.animals.length;
-    }
-
-}
-
-const collection = new CollectionAnimals;
-
-
-class Cow {
-
-}
-
-class Goat {
-
-}
-
-class Cat {
-
-}
-
-class Dog {
-
-}
-
-
-collection.createAmountAnimal('cow', 2, 3, 5);
-collection.createAmountAnimal('goat', 2, 4);
-collection.createAmountAnimal('cat', 3);
-collection.createAmountAnimal('dog', 2);
-
-console.log(collection);
-console.log(collection.countTotal());
+btn.addEventListener('click', function() {
+  body.classList.toggle('switch');
+})
 
 
 
