a251ee8c3d2625b58b45d10da71b1ad3b40d6739
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-06-28 23:50:06 +0200

    Did not work out

--- index.js
+++ index.js
@@ -2,13 +2,28 @@
 
 
 
-const elem = document.querySelector('input');
-const res = document.querySelector('#res');
 
-elem.addEventListener('blur', function() {
-  res.textContent = Number(elem.value) ** 2;
-})
+const elem = document.querySelector('#elem');
+const btn = document.querySelector('#btn');
+const body = document.querySelector('body');
+
+let days = ['вс', 'пн', 'вт', 'ср', 
+	'чт', 'пт', 'сб'];
+
+const date = new Date();
 
 
+function func(event) {
+  if (date.getDay() % 2 !== 0) {
+    event.stopPropagation();
+    console.log('!!!');
+  }
+
+}
 
 
+btn.addEventListener('click', function() {
+  console.log(`Сегодня ${days[date.getDay()]}`);
+});
+
+body.addEventListener('click', func, {capture: true});
