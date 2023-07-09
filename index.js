d2c3fa04673c676fad299087efc6697b3ce0eaee
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-07-09 22:35:29 +0200

    Home work promise

--- index.js
+++ index.js
@@ -1,54 +1,118 @@
-'use strict';
+`use strict`
 
 
 
 
 
+const root = document.querySelector('#root');
 
 
-const doing = document.querySelector('#doing');
-const btn = document.querySelector('#btn');
-const list = document.querySelector('#list');
-
-
-btn.addEventListener('click', createList);
-
-
-
-function createList() {
-  const elem = document.createElement('li');
-
-  const inp = document.createElement('input');
-  inp.classList.add('text');
-
-  const redact = document.createElement('button');
-  const del = document.createElement('button');
-
-
-  inp.value = doing.value;
-  inp.setAttribute('disabled', 'true');
-  doing.value = '';
-  redact.textContent = 'Redact';
-  del.textContent = 'Remove';
-
-  list.append(elem);
-  elem.append(inp, redact, del);
-  
-  
-  redact.addEventListener('click', function() {
-    inp.removeAttribute('disabled');
-    inp.focus();
-  })
-
-  inp.addEventListener('blur', function() {
-    inp.setAttribute('disabled', 'true');
-  })
-
-  del.addEventListener('click', function() {
-    elem.remove();
-  })
-
+function imgLoadHandler({target}) {
+    console.log('image successfully loaded');
+    const parentWrapper = document.getElementById(`wrapper${target.id}`);
+    parentWrapper.append(target);
 }
+
+function imgErrorHandler({target}) {
+    target.remove();
+    console.log('image loading has error');
+}
+
+
+
+
+function createCard(product) {
+    const imgWrapper = createImgWrapper(product);
+
+    const h2 = createElem('h2', { elemClass: ['title'] }, `${product.title}`);
+    const p = createElem('p', { elemClass: ['description'] }, `${product.description}`);
+    const price = createElem('p', { elemClass: ['price'] }, `${product.price}$`);
+    price.style.color = stringToColor(`${product.brand} ${product.title} ${product.category}`);
+    const rating = createElem('p', { elemClass: ['rating'] }, `Raiting: ${product.rating}`);
+    const btn = createElem('button', { elemClass: ['add-product'] }, 'Add');
+    btn.style.backgroundColor = stringToColor(`${product.brand} ${product.title} ${product.category}`);
+
+    return createElem('article', { elemClass: ['card'] }, imgWrapper, h2, p, price, rating, btn);
+}
+
+
+
+function createElem(type, { elemClass }, ...childs) {
+    const elem = document.createElement(type);
+
+    elem.classList.add(...elemClass);
+    elem.append(...childs);
+
+    return elem;
+}
+
+
+
+
+function createImgWrapper(product) {
+    const imgWrapper = createElem('div', { elemClass: ['img-wrapper'] })
+
+    imgWrapper.setAttribute('id', `wrapper${product.id}`);
+    imgWrapper.style.backgroundColor = stringToColor(`${product.brand} ${product.title} ${product.category}`);
+
+    // const img = createImg(product);      /* ???? */
+
+    return imgWrapper;
+}
+
+
+function createImg(product) {
+    const img = document.createElement('img');
+
+    const [{images}] = product;             /* ????? */
+
+    img.setAttribute('src', images[0]);  /*  ??????? */
+    img.setAttribute('alt', product.brand);
+    img.setAttribute('id', `${product.id}`);
+    img.classList.add('card-img');
+
+    img.addEventListener('load', imgLoadHandler);
+    img.addEventListener('error', imgErrorHandler);
+
+    return img;
+}
+
+function stringToColor(str) {
+    let hash = 0;
+    str.split('').forEach(char => {
+        hash = char.charCodeAt(0) + ((hash << 5) - hash)
+    })
+    let colour = '#'
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff
+        colour += value.toString(16).padStart(2, '0')
+    }
+    return colour
+}
+
+
+
+
+const prom = fetch('https://dummyjson.com/products')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const { products } = data;
+        const [{images}] = products;
+        console.log(images);
+
+        const productsArr = products.map((product) => { return createCard(product); })
+        root.append(...productsArr);
+    })
+
+
+
+
+
+
+
 
 
 
